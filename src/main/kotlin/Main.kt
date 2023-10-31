ed2a4c2a74464af86709484a85447264da2061f4
dominik_weber <dominikweber.herne@gmail.com>
2023-10-31 13:53:40 +0100

    einige dinge funktionieren , andere noch nicht aber ich bin der Lösung auf der Spur, danach geht es schneller

--- src/main/kotlin/Main.kt
+++ src/main/kotlin/Main.kt
@@ -1,12 +1,18 @@
 
 fun main() {
-
     var heroOne = NatureMutant("Domi")
     var enemyOne = Endboss("Malte")
+    var roundCounter = 1
 
-    heroOne.thunderStorm(enemyOne)
-    println(enemyOne.healthPower)
 
+    while (enemyOne.healthPower>0){
+        heroOne.poisonRoots(enemyOne,roundCounter)
+        println(enemyOne.healthPower)
+        heroOne.thunderStorm(enemyOne)
+        println(enemyOne.healthPower)
+        println("Das war Runde $roundCounter")
+        roundCounter++
+    }
 }
 
 
--- src/main/kotlin/NatureMutant.kt
+++ src/main/kotlin/NatureMutant.kt
@@ -11,17 +11,31 @@ class NatureMutant(override var name: String ="Gaia", override var healthPower: 
      * @return enemy.healthPower
      */
     fun thunderStorm(enemy: Enemy){
+        if (enemy.healthPower > 0){
+            println("Es ziehen dunkle Wolken auf über Gaia und ihren Freunden...")
+            var shocksInt = (1..3).random()
+            println("$shocksInt Blitze haben ${enemy.name} getroffen")
+            var abzug = 0
+            repeat(shocksInt) {
+                if (enemy.healthPower >= 200){
+                    abzug += enemy.healthPower / 100 * (5..8).random()
+                }else{
+                    println("${enemy.name} hat unter 200 Lebenspunkte, der Abzug durch einen Blitz beträgt nur noch 10 Lebenspunkte")
+                    abzug += 10
+                }
+            }
+            if (enemy.healthPower >= abzug) {
+                println("$abzug wurden ${enemy.name} von seiner HP abgezogen")
+                enemy.healthPower -= abzug.toInt()
+            } else {
+                println("Die Blitze haben Ihm den Rest gegeben")
+                println("${enemy.name} hat keine Lebensenergie mehr")
+            }
 
-        println("Es ziehen dunkle Wolken auf über Gaia und ihren Freunden...")
-        var shocksInt = (1..3).random()
-        println("$shocksInt Blitze haben $enemy getroffen")
-        var abzug = 0
-        repeat(shocksInt) {
-            abzug += (enemy.healthPower / 100 * (5..8).random())/100 *damagePower
         }
-        println("$abzug wurden ${enemy.name} von seiner HP abgezogen")
-        enemy.healthPower -= abzug.toInt()
-        //return enemy.healthPower
+
+
+
     }
 
 
@@ -30,17 +44,37 @@ class NatureMutant(override var name: String ="Gaia", override var healthPower: 
      * Zusätzlich verursacht diese Methode zwischen 3 und 5 Runden einen Gift-Schaden von je 7 % Lebensenergie.
      *@param enemy
      */
-    fun poisonRoots(enemy: Enemy){
+    fun poisonRoots(enemy: Enemy,round:Int){
 
         // random rounds, die die Lebenspunkte abgezogen werden
-        var randomRounds: Int = (3..5).random()
-        var deduction = enemy.healthPower / 100 * 7
+        var randomRounds = (1..5).random()
 
+        println("Die Wurzeln haben ${enemy.name} fest im Giff, für ${randomRounds+1} Runden werden ihm Lebenspunkte abgezogen")
 
+        var endRound = round+randomRounds
 
-        //  enemy damagePower -50 % für 1 Runde
+        for (i in round..endRound) {
+            var abzug = 0
+            if (enemy.healthPower == 0) {
+                println("${enemy.name} hat keine Lebenspunkte mehr")
+                break
+            } else if (enemy.healthPower <= 200) {
+                println("${enemy.name} hat unter 200 Lebenspunkte, ab jetzt ziehen die Wurzeln nur noch 15 Lebenspunkte ab")
+                abzug += 15
+            } else {
+                abzug += enemy.healthPower / 100 * 7
+                println("${enemy.name} hat über 200 Lebenspunkte, ihm werden 7% Lebensenergie abgezogen")
+            }
+            if (enemy.healthPower >= abzug) {
+                enemy.healthPower -= abzug
+            } else
+                enemy.healthPower = 0
+        }
 
-
+        for (i in round..round){
+            enemy.damagePower/100 * 50
+            println("${enemy.name} ist geschwächt, er hat nur die hälfte seiner Angriffskraft ")
+        }
     }
 
     /**
